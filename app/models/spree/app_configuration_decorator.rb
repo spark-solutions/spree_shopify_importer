7da4a5f861f01d6adb5916bc6954c09e42ec37e4
Joasia <joasia@Joasias-MacBook-Pro-2.local>
2019-10-14 07:29:36 +0200

    add API version

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -3,6 +3,7 @@ Spree::AppConfiguration.class_eval do
   preference :shopify_password, :string
   preference :shopify_shop_domain, :string
   preference :shopify_token, :string
+  preference :shopify_api_version, :string
   preference :shopify_rescue_limit, :integer, default: 5
   preference :shopify_current_credentials, :hash
   preference :shopify_import_queue, :string, default: 'default'
