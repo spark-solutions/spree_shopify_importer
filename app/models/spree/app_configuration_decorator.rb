6166f0d328d0d0949f5d1542d310473a938fcbf8
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-10-24 11:22:24 +0200

    Changes class_eval to prepend in app_configuration_decorator

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,10 +1,13 @@
-Spree::AppConfiguration.class_eval do
-  preference :shopify_api_key, :string
-  preference :shopify_password, :string
-  preference :shopify_shop_domain, :string
-  preference :shopify_token, :string
-  preference :shopify_api_version, :string
-  preference :shopify_rescue_limit, :integer, default: 5
-  preference :shopify_current_credentials, :hash
-  preference :shopify_import_queue, :string, default: 'default'
+module Spree::AppConfigurationDecorator
+  def self.prepended(base)
+    base.preference :shopify_api_key, :string
+    base.preference :shopify_password, :string
+    base.preference :shopify_shop_domain, :string
+    base.preference :shopify_token, :string
+    base.preference :shopify_rescue_limit, :integer, default: 5
+    base.preference :shopify_current_credentials, :hash
+    base.preference :shopify_import_queue, :string, default: 'default'
+  end
 end
+
+Spree::AppConfiguration.prepend Spree::AppConfigurationDecorator
