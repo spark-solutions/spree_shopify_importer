624ea2493f3b55ec12cb0807282054f4f8d2de15
Joasia <joasia@Joasias-MacBook-Pro-2.local>
2019-11-08 11:29:37 +0100

    fix importers/taxons tests

--- spec/services/spree_shopify_importer/data_savers/taxons/taxon_updater_spec.rb
+++ spec/services/spree_shopify_importer/data_savers/taxons/taxon_updater_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe SpreeShopifyImporter::DataSavers::Taxons::TaxonUpdater, type: :service do
   subject { described_class.new(shopify_data_feed, spree_taxon) }
 
-  before { ShopifyAPI::Base.site = 'https://api_key:passowrd@shop_domain.myshopify.com/admin' }
+  before { authenticate_with_shopify }
 
   describe '#update!', vcr: { cassette_name: 'shopify/base_custom_collection' } do
-    let(:shopify_custom_collection) { ShopifyAPI::CustomCollection.find(388_567_107) }
+    let(:shopify_custom_collection) { ShopifyAPI::CustomCollection.find(440_333_380) }
     let(:shopify_data_feed) { create(:shopify_data_feed, data_feed: shopify_custom_collection.to_json) }
     let(:taxonomy) { create(:taxonomy, name: I18n.t('shopify_custom_collections')) }
 
@@ -38,7 +38,7 @@ describe SpreeShopifyImporter::DataSavers::Taxons::TaxonUpdater, type: :service 
         let!(:product_data_feed) do
           create(:shopify_data_feed,
                  shopify_object_type: 'ShopifyAPI::Product',
-                 shopify_object_id: '9884552707',
+                 shopify_object_id: '11055169028',
                  spree_object: spree_product)
         end
 
