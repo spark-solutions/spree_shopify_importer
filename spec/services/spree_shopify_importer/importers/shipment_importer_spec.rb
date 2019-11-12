39c600a40d35660406c975465683ddb19688ac6c
Joasia <joasia@Joasias-MacBook-Pro-2.local>
2019-11-12 07:10:19 +0100

    add authentication before find order

--- spec/services/spree_shopify_importer/importers/shipment_importer_spec.rb
+++ spec/services/spree_shopify_importer/importers/shipment_importer_spec.rb
@@ -1,14 +1,13 @@
 require 'spec_helper'
 
-describe SpreeShopifyImporter::Importers::ShipmentImporter, type: :service do
+RSpec.describe SpreeShopifyImporter::Importers::ShipmentImporter, type: :service do
   let!(:parent_feed) { create(:shopify_data_feed, data_feed: shopify_order.to_json) }
   let!(:spree_order) { create(:order) }
-
   subject { described_class.new(fulfillment, parent_feed, spree_order) }
-  before  { authenticate_with_shopify }
-  after   { ShopifyAPI::Base.clear_session }
+  before { authenticate_with_shopify }
 
   describe '#import!', vcr: { cassette_name: 'shopify/importers/shipment_importer/import' } do
+    authenticate_with_shopify
     let(:shopify_order) { ShopifyAPI::Order.find(5_182_437_124) }
     let(:fulfillment) { shopify_order.fulfillments.first }
 
