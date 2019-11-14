ec4627f97ca81be08352a48bc11ac0684676ec1e
jeniaefimov <e.effimov@mail.ru>
2019-11-14 13:27:33 +0100

    Fix integrations spec and shipment importer spec

--- spec/services/spree_shopify_importer/importers/shipment_importer_spec.rb
+++ spec/services/spree_shopify_importer/importers/shipment_importer_spec.rb
@@ -3,11 +3,12 @@ require 'spec_helper'
 RSpec.describe SpreeShopifyImporter::Importers::ShipmentImporter, type: :service do
   subject { described_class.new(fulfillment, parent_feed, spree_order) }
 
+  authenticate_with_shopify
+
   let!(:parent_feed) { create(:shopify_data_feed, data_feed: shopify_order.to_json) }
   let!(:spree_order) { create(:order) }
 
   describe '#import!', vcr: { cassette_name: 'shopify/importers/shipment_importer/import' } do
-    authenticate_with_shopify
     let(:shopify_order) { ShopifyAPI::Order.find(5_182_437_124) }
     let(:fulfillment) { shopify_order.fulfillments.first }
 
