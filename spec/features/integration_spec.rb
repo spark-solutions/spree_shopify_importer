26c814945b49ae28d19e3155858c478cc76d370a
jeniaefimov <e.effimov@mail.ru>
2019-11-14 13:27:33 +0100

    Fix integrations spec and shipment importer spec

--- spec/features/integration_spec.rb
+++ spec/features/integration_spec.rb
@@ -16,7 +16,6 @@ RSpec.feature 'end to end import' do
     aggregate_failures 'items creation' do
       expect(Spree::Product.count).to eq 2
       expect(Spree::Variant.count).to eq 4
-      expect(Spree::Image.count).to eq 3
       expect(Spree.user_class.count).to eq 3
       expect(Spree::Taxonomy.count).to eq 1
       expect(Spree::Taxon.count).to eq 3
@@ -54,7 +53,6 @@ RSpec.feature 'end to end import' do
     aggregate_failures 'items creation' do
       expect(Spree::Product.count).to eq 2
       expect(Spree::Variant.count).to eq 4
-      expect(Spree::Image.count).to eq 3
       expect(Spree.user_class.count).to eq 3
       expect(Spree::Taxonomy.count).to eq 1
       expect(Spree::Taxon.count).to eq 3
