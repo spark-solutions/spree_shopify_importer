2ea739a55e8ca1f8cd448738de1d8a1133df73a4
jeniaefimov <e.effimov@mail.ru>
2019-11-19 16:05:00 +0100

    Remove pry

--- config/initializers/shopify_api_initializer.rb
+++ config/initializers/shopify_api_initializer.rb
@@ -1,3 +1,2 @@
-binding.pry
 ShopifyAPI::Base.api_version = ENV['SHOPIFY_API_VERSION']
 ShopifyAPI::Base.site = "https://#{ENV['SHOPIFY_API_KEY']}:#{ENV['SHOPIFY_API_PASSWORD']}@#{ENV['SHOPIFY_API_DOMAIN']}/admin/"
