0693de815893f6db0297798ed10094e884dcc36f
Joasia <consilis@wp.pl>
2020-01-24 15:55:04 +0100

    set currency setting

--- app/services/spree_shopify_importer/importers/shop_importer.rb
+++ app/services/spree_shopify_importer/importers/shop_importer.rb
@@ -3,10 +3,17 @@ module SpreeShopifyImporter
     class ShopImporter < BaseImporter
       def import!
         process_data_feed
+        set_currency
       end
+
+      private
 
       def shopify_class
         ShopifyAPI::Shop
+      end
+
+      def set_currency
+        Spree::Config[:currency] = JSON.parse(@resource)['currency']
       end
     end
   end
